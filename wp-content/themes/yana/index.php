6a79948f33dc7d57cecbc2b40211bc0acdcb99b8
Arthur Immanuel <arthurimmanuelsidjabat@gmail.com>
2023-03-25 13:33:45 +0700

    Update Plugins Duplicator, Package Bootstrap 5.3, Views

--- wp-content/themes/yana/index.php
+++ wp-content/themes/yana/index.php
@@ -1,20 +1,48 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
-  </head>
 
-  <body <?php body_class(); ?>>
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#212529">
+    <meta name="msapplication-TileColor" content="#212529">
+    <meta name="theme-color" content="#212529">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <?php do_action('get_header'); ?>
 
     <div id="app">
-      <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
+        <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
     </div>
 
     <?php do_action('get_footer'); ?>
     <?php wp_footer(); ?>
-  </body>
+
+    <script>
+        WebFontConfig = {
+            google: {
+                families: ['Noto+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700','Lora:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700']
+            }
+        };
+
+        (function(d) {
+            var wf = d.createElement('script'),
+                s = d.scripts[0];
+            wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+            wf.async = true;
+            s.parentNode.insertBefore(wf, s);
+        })(document);
+    </script>
+    
+</body>
+
 </html>
--- wp-content/themes/yana/resources/views/additional-content/social-icon-end.blade.php
+++ wp-content/themes/yana/resources/views/additional-content/social-icon-end.blade.php
@@ -1,16 +1,16 @@
 <ul class="social-icon-container list-group list-unstyled list-group-horizontal d-flex justify-content-end">
     <li class="social-icon">
-        <a class="text-decoration-none" target="_blank" href="https://www.facebook.com/" rel="nofollow">
+        <a class="link-primary text-decoration-none" target="_blank" href="https://www.facebook.com/" rel="nofollow">
             <i class="bi bi-facebook"></i>
         </a>
     </li>
     <li class="social-icon">
-        <a class="text-decoration-none" target="_blank" href="https://twitter.com/" rel="nofollow">
+        <a class="link-sky-blue text-decoration-none" target="_blank" href="https://twitter.com/" rel="nofollow">
             <i class="bi bi-twitter"></i>
         </a>
     </li>
     <li class="social-icon">
-        <a class="text-decoration-none" target="_blank" href="https://www.instagram.com/" rel="nofollow">
+        <a class="link-danger text-decoration-none" target="_blank" href="https://www.instagram.com/" rel="nofollow">
             <span class="bi bi-instagram"></span>
         </a>
     </li>
